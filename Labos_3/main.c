28d8b457345861f6c085403a2c8dd698e7c70366
Niko Rimac <nrimac00@gmail.com>
2024-05-12 18:54:11 +0200

    Fixed major threads issue, shouldn't be a problem anymore

--- Labos_3/main.c
+++ Labos_3/main.c
@@ -4,106 +4,150 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define BUD 4
-#define BRD 6
+#define BUD 1
+#define BRD 2
 #define BID 3
-#define VEL_MS 7
+#define VEL_MS 5
 
+char UMS[BRD][VEL_MS];
+char IMS[BID][VEL_MS];
+
+pthread_t threads[BUD + BRD + BID];
 sem_t writeSem;
+sem_t readUMSSem[BRD];
 
+//initialize threads
 void stvoriUlazneDretve();
 void stvoriRadneDretve();
 void stvoriIzlazneDretve();
 
+//thread functions
 int dohvatiUlaz();
 int obradiUlaz();
+void obradi(int id, int podatak, int* r, int* t);
 
+//helper functions
 void fillMS();
 void printUMS();
 void printIMS();
 
 void init() {
+    fillMS();
     sem_init(&writeSem, 0, 1);
+
+    for (int i = 0; i < BRD; i++) {
+        sem_init(&readUMSSem[i], 0, 0);
+    }
 }
 
 int main() {
     init();
 
     stvoriUlazneDretve();
+    stvoriRadneDretve();
+    stvoriIzlazneDretve();
+
+    for (int i = 0; i < BUD + BRD + BID; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
 }
 
-void fillMS(char* UMS, char* IMS) {
+void fillMS() {
     for (int i = 0; i < BRD; i++) {
         for (int j = 0; j < VEL_MS; j++) {
-            *(UMS + i * VEL_MS + j) = '-';
+            UMS[i][j] = '-';
         }
     }
 
     for (int i = 0; i < BID; i++) {
         for (int j = 0; j < VEL_MS; j++) {
-            *(IMS + i * VEL_MS + j) = '-';
+            IMS[i][j] = '-';
         }
     }
 }
 
-void printUMS(char* UMS) {
+void printUMS() {
     printf("UMS[]: ");
     for (int i = 0; i < BRD; i++) {
-        for (int j = 0; j < VEL_MS; j++) printf("%c", *(UMS + i * VEL_MS + j));
+        for (int j = 0; j < VEL_MS; j++) printf("%c", UMS[i][j]);
         printf(" ");
     }
     printf("\n");
 }
 
-void printIMS(char* IMS) {
+void printIMS() {
     printf("IMS[]: ");
     for (int i = 0; i < BID; i++) {
-        for (int j = 0; j < VEL_MS; j++) printf("%c", *(IMS + i * VEL_MS + j));
+        for (int j = 0; j < VEL_MS; j++) printf("%c", IMS[i][j]);
         printf(" ");
     }
     printf("\n");
 }
 
-int dohvatiUlaz(int id) {
+int dohvatiUlaz(const int id) {
     srand(time(NULL) + id);
     return 'A' + rand() % 26;
 }
 
-int obradiUlaz(int id, int ulaz) {
+int obradiUlaz(const int id, const int ulaz) {
     return (ulaz + id) % BRD;
 }
 
+void obradi(const int id, const int podatak, int* r, int* t) {
+    *r = podatak + 32;
+}
+
 void *ulaz(void * threadId) {
-    char UMS[BRD][VEL_MS];
-    char IMS[BID][VEL_MS];
     const int id = (int) threadId;
 
-    fillMS(UMS, IMS);
-    stvoriRadneDretve();
+    int ulaz[BRD];
 
     while (1) {
+        //K.O.
+        sem_wait(&writeSem);
         const int U = dohvatiUlaz(id);
         const int T = obradiUlaz(id, U);
 
-        //K.O.
-        sem_wait(&writeSem);
-        UMS[T][0] = U;
+        UMS[T][ulaz[T]] = U;
+        ulaz[T] = (ulaz[T] + 1) % VEL_MS;
+        sem_post(&readUMSSem[T]);
+
+
         printf("U%d: dohvatiUlaz(%d) => '%c', obradiUlaz(%c) => %d\n", id, id, U, U, T);
-        printUMS(UMS);
-        printIMS(IMS);
+        printUMS();
+        printIMS();
         printf("\n");
         sem_post(&writeSem);
         //Kraj K.O.
 
-        sleep(5);
+        sleep(1);
     }
 }
 
 void *rad(void * threadId) {
-    int id = (int) threadId;
-    //printf("id: %d\n", id);
+    int izlaz[BRD];
+    const int id = (int) threadId;
+
+    while (1) {
+        //K.O.
+        sem_wait(&readUMSSem[id]);
+        const int P = UMS[id][izlaz[id]];
+        UMS[id][izlaz[id]] = "-";
+        izlaz[id] = (izlaz[id] + 1) % VEL_MS;
+        //obradi podatak
+        int r, t;
+        obradi(id, P, &r, &t);
+
+        sem_wait(&writeSem);
+        printf("R%d: UMS[%d][%d] => '%c'\n", id, id, izlaz[id], P);
+        printUMS();
+        printIMS();
+        printf("\n");
+        sem_post(&writeSem);
+        //Kraj K.O.
+    }
 }
 
 void *izlaz(void * threadId) {
@@ -111,43 +155,25 @@ void *izlaz(void * threadId) {
 }
 
 void stvoriUlazneDretve() {
-    pthread_t threads[BUD];
-
     for (int i = 0; i < BUD; i++) {
         pthread_t ulaznaDretva;
         pthread_create(&ulaznaDretva, NULL, ulaz, i);
         threads[i] = ulaznaDretva;
     }
-
-    for (int i = 0; i < BUD; i++) {
-        pthread_join(threads[i], NULL);
-    }
 }
 
 void stvoriRadneDretve() {
-    pthread_t threads[BRD];
-
     for (int i = 0; i < BRD; i++) {
         pthread_t radnaDretva;
         pthread_create(&radnaDretva, NULL, rad, i);
-        threads[i] = radnaDretva;
-    }
-
-    for (int i = 0; i < BRD; i++) {
-        pthread_join(threads[i], NULL);
+        threads[i + BUD] = radnaDretva;
     }
 }
 
 void stvoriIzlazneDretve() {
-    pthread_t threads[BID];
-
     for (int i = 0; i < BID; i++) {
         pthread_t izlaznaDretva;
         pthread_create(&izlaznaDretva, NULL, izlaz, i);
-        threads[i] = izlaznaDretva;
-    }
-
-    for (int i = 0; i < BID; i++) {
-        pthread_join(threads[i], NULL);
+        threads[i + BRD + BUD] = izlaznaDretva;
     }
 }
